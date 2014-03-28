d119ab03b9a5422529b450875954a4542c5318ea
jwaimann-workshopgit <jwaimann@makingsense.com>
2014-03-28 14:33:52 -0300

    Change welcome message

--- DemoApp/DemoApp/Controllers/HomeController.cs
+++ DemoApp/DemoApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace DemoApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Geeting ready for git!";
 
             return View();
         }
